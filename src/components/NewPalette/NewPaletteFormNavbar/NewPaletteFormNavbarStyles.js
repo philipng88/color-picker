394c4bd7efc7d32071beb1b3275970f827b54b32
philipng88 <philiplyng@gmail.com>
2020-05-30 15:16:16 -0600

    remove padding from color boxes container in new palette form and hide hamburger icon when drawer is open

--- src/components/NewPalette/NewPaletteFormNavbar/NewPaletteFormNavbarStyles.js
+++ src/components/NewPalette/NewPaletteFormNavbar/NewPaletteFormNavbarStyles.js
@@ -5,6 +5,9 @@ const NewPaletteFormNavbarStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
   },
+  hide: {
+    display: "none",
+  },
   appBar: {
     transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
